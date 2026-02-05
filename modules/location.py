1751198e20d4a3d8d3fee89b220c1a33c9a7e871
Aditya Karun <114733642+AdityaKarun@users.noreply.github.com>
2026-02-05 10:07:33 +0530

    add logging in location

--- modules/location.py
+++ modules/location.py
@@ -1,4 +1,7 @@
+import logging
 import requests
+
+logger = logging.getLogger(__name__)
 
 def get_location():
     """
@@ -10,22 +13,48 @@ def get_location():
     Returns:
         str: City name if available, otherwise an error message.
     """
-    try:
-        response = requests.get("https://ipinfo.io/json", timeout=10)
-        data = response.json()
+    url = "https://ipinfo.io/json"
+    timeout=10
+    fallback = "Could not fetch location data"
 
-        city = data.get("city")
+    try:
+        response = requests.get(url, timeout=timeout)
+        logger.debug(
+            "API request to IPinfo | URL=%s Timeout=%s",
+            url,
+            timeout
+        )
+        
+        logger.debug(
+            "API response from IPinfo | Status=%s",
+            response.status_code,
+        )
+        
+        # Raises exception for non-2xx responses
+        response.raise_for_status()
+
+        # Decode JSON response from IPinfo into Python objects
+        data = response.json()
+        logger.debug("Parsed JSON response from IPinfo: %s", data)
+
+        city = data.get("city", fallback)
+        logger.debug("Detected location: %s", city)
 
         return city
     
-    except Exception as e:
-        return f"Could not fetch location data."
+    except requests.RequestException as e:
+        # Network issues, timeouts, or non-2xx HTTP responses
+        logger.warning("HTTP/network error calling IPinfo API: %s", e)
+        return fallback
+    
+    except Exception:
+        # Unexpected programming or runtime error
+        logger.exception("Unexpected error while fetching location")
+        return fallback
 
 
 if __name__ == "__main__":
-
-    city = get_location()
-    if city:
-        print(f"You are currently in: {city}")
-    else:
-        print("Unable to fetch location info.")
+    from core.logger_config import setup_logging
+    
+    setup_logging()
+    get_location()
